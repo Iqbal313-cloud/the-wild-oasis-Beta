acc62dd8f6fcd73ea237d33157da11cb0ad41a8a
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-09-10 20:01:16 +0600

    at the end of 332

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
+            <div>
+              <Heading as="h2">The wild Oasis</Heading>
+              <Button> check in </Button>
+              <Button variation="secondary" size="small">
+                {" "}
+                check out{" "}
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
