41eb47d0e6e8d2f225e58f475eb8559858ade4e8
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-09-11 01:01:56 +0600

    at the end of section 26

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://jistleobwembmrmeskdj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imppc3RsZW9id2VtYm1ybWVza2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5ODcxNzQsImV4cCI6MjA0MTU2MzE3NH0.humYO50rBUxo7XrJ3E1Y-iBpV1MJw7aky9YFLYydDK8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
