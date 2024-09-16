fae1926588cb55f2c494567712a52e3a89768fef
Iqbal <muhammadiqbalxyz251@gmail.com>
2024-09-16 23:35:56 +0600

    at the end of 386

---
+++ src/features/check-in-out/useCheckin.js
@@ -0,0 +1,26 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function useCheckin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-in",
+        isPaid: true,
+      }),
+
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} sucessfully checked in`);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("Ther was an error while checking in"),
+  });
+
+  return { checkin, isCheckingIn };
+}
